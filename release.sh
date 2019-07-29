035b4cf41e0434c3edc56a11baeaa4b0458c0e01
Jay Fong <fjc0kb@gmail.com>
2019-07-30 00:35:16 +0800

    fix: 发布脚本

--- release.sh
+++ release.sh
@@ -2,5 +2,5 @@
 
 YAPI_VERSION=$(grep -o "YAPI_VERSION=.*" Dockerfile | sed "s/YAPI_VERSION=//g")
 
-git tag $YAPI_VERSION
-git push origin --follow-tags
+git tag "$YAPI_VERSION"
+git push origin --tags
