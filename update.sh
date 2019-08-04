bf14efdfefd2f4af6859c39acbcc0c2869ee55d7
Jay Fong <fjc0kb@gmail.com>
2019-08-04 17:58:59 +0800

    chore: update

--- update.sh
+++ update.sh
@@ -9,6 +9,9 @@ git remote add origin https://${GITHUB_TOKEN}@github.com/fjc0k/docker-YApi.git &
 PROJECT_LATEST_TAG=$(wget -q -O- https://api.github.com/repos/fjc0k/docker-YApi/tags | grep '"name":' | sed -E 's#.*"(.+)".*#\1#' | head -n1)
 YAPI_LATEST_TAG=$(wget -q -O- https://api.github.com/repos/YMFE/yapi/tags | grep '"name":' | sed -E 's#.*"v(.+)".*#\1#' | head -n1)
 
+echo "docker-YApi: ${PROJECT_LATEST_TAG}"
+echo "YApi: ${YAPI_LATEST_TAG}"
+
 if [ $PROJECT_LATEST_TAG != $YAPI_LATEST_TAG ]; then
   sed -i '' "s#YAPI_VERSION=.*#YAPI_VERSION=$YAPI_LATEST_TAG#" Dockerfile
   git add Dockerfile
