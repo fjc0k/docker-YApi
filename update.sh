281cef8e8f57ca064413a5058496445e6a5fc806
Jay Fong <fjc0kb@gmail.com>
2019-08-07 14:35:49 +0800

    fix(ci): 自动更新 YApi

--- update.sh
+++ update.sh
@@ -9,13 +9,13 @@ git remote add origin https://${GITHUB_TOKEN}@github.com/fjc0k/docker-YApi.git &
 PROJECT_LATEST_TAG=$(wget -q -O- https://api.github.com/repos/fjc0k/docker-YApi/tags | grep '"name":' | sed -E 's#.*"(.+)".*#\1#' | head -n1)
 YAPI_LATEST_TAG=$(wget -q -O- https://api.github.com/repos/YMFE/yapi/tags | grep '"name":' | sed -E 's#.*"v(.+)".*#\1#' | head -n1)
 
-echo "docker-YApi: ${PROJECT_LATEST_TAG}"
-echo "YApi: ${YAPI_LATEST_TAG}"
+echo "YApi: $YAPI_LATEST_TAG"
+echo "docker-YApi: $PROJECT_LATEST_TAG"
 
 if [ $PROJECT_LATEST_TAG != $YAPI_LATEST_TAG ]; then
   sed -i'' "s#YAPI_VERSION=.*#YAPI_VERSION=$YAPI_LATEST_TAG#" Dockerfile
   git add Dockerfile
   git commit -m "feat: 升级 YApi 版本 [$YAPI_LATEST_TAG]"
   git tag -a "$YAPI_LATEST_TAG" -m "YApi v$YAPI_LATEST_TAG"
-  git push origin --follow-tags --quiet &> /dev/null
+  git push --set-upstream origin master --follow-tags --quiet &> /dev/null
 fi
